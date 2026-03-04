5d321a91f0299caeb52f6dd7a2420e1224a8d689
Felipe Brandão <felipe.b31@gmail.com>
2026-03-04 15:20:39 -0300

    Inicialize a estrutura do projeto com a configuração do Spring Boot

---
+++ src/main/java/br/com/felipebrandao/vidya/VidyaApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipebrandao.vidya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidyaApplication.class, args);
+    }
+
+}
